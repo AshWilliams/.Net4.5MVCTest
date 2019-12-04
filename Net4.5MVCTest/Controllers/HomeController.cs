5b123539a02e6c826fa28d3ea960851e081ac4d9
Robert Rozas Navarro <rorozasn@microsoft.com>
2019-12-04 11:09:07 -0600

    sitio de prueba ok

---
+++ Net4.5MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net4._5MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public void Exception1()
+        {
+            throw new System.InvalidOperationException("Logfile cannot be read-only");
+        }
+
+        public void Exception2()
+        {
+            throw new System.ArgumentException("Parameter cannot be null", "original");
+        }
+
+        public void Exception3()
+        {
+            throw new Exception("division by zero");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
